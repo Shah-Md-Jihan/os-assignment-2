056401a5af77b9ede89843e9d45640c86bb4d905
Shah Md Jihan <shahmohammadjihan@gmail.com>
2024-05-17 16:52:39 +0600

    problem solving 1 done

---
+++ string.php
@@ -0,0 +1,27 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+function vowelChecker($words){
+    function count_vowels($string) {
+
+    $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+    $count = 0;
+
+    $chars = str_split($string);
+
+    foreach ($chars as $char) {
+        if (in_array($char, $vowels)) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+
+    foreach($words as $word){
+        echo "Original String: ".$word.", Vowel Count: ".count_vowels($word)." Reversed String: ".strrev($word)."\n";
+    }
+}
+
+vowelChecker($strings);
